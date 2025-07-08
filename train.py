7a4f7a8497f4fc3c5f26b75055668c6d59de3ebc
yang cao <st186635@stud.uni-stuttgart.de>
2025-07-08 15:25:28 +0200

    add ignore add hydra and wandb

--- train.py
+++ train.py
@@ -2,10 +2,14 @@ import random
 import os
 import torch
 from path import Path
+import wandb
+import hydra
+from omegaconf import DictConfig
 from src.models import model
 from src.pipelines import dataset
 from src.utils import utils
 from src.pipelines.args import parse_args
+
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
@@ -25,8 +29,8 @@ def pointnetloss(outputs, labels, m3x3, m64x64, alpha=0.0001):
     return criterion(outputs, labels) + alpha * (torch.norm(diff3x3) + torch.norm(diff64x64)) / float(bs)
 
 
-def train(args):
-    path = Path(args.root_dir)
+def train(cfg):
+    path = Path(cfg.root_dir)
 
     folders = [dir for dir in sorted(os.listdir(path)) if os.path.isdir(path / dir)]
     classes = {folder: i for i, folder in enumerate(folders)}
@@ -42,7 +46,7 @@ def train(args):
     print(device)
     pointnet = model.PointNet()
     pointnet.to(device)
-    optimizer = torch.optim.Adam(pointnet.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(pointnet.parameters(), lr=cfg.lr)
 
     train_ds = dataset.PointCloudData(path, transform=train_transforms)
     valid_ds = dataset.PointCloudData(path, valid=True, folder='test', transform=train_transforms)
@@ -50,16 +54,27 @@ def train(args):
     print('Valid dataset size: ', len(valid_ds))
     print('Number of classes: ', len(train_ds.classes))
 
-    train_loader = DataLoader(dataset=train_ds, batch_size=args.batch_size, shuffle=True)
-    valid_loader = DataLoader(dataset=valid_ds, batch_size=args.batch_size * 2)
+    train_loader = DataLoader(dataset=train_ds, batch_size=cfg.batch_size, shuffle=True)
+    valid_loader = DataLoader(dataset=valid_ds, batch_size=cfg.batch_size * 2)
 
     try:
-        os.mkdir(args.save_model_path)
-    except OSError as error:
-        print(error)
+        os.mkdir(cfg.save_model_path)
+    except OSError:
+        pass
+
+
+    wandb.init(
+        project="pointnet-project",
+        config={
+            "batch_size": cfg.batch_size,
+            "lr": cfg.lr,
+            "epochs": cfg.epochs
+        },
+        name=f"run_{wandb.util.generate_id()}"
+    )
 
     print('Start training')
-    for epoch in range(args.epochs):
+    for epoch in range(cfg.epochs):
         pointnet.train()
         running_loss = 0.0
         for i, data in enumerate(train_loader, 0):
@@ -71,34 +86,37 @@ def train(args):
             loss.backward()
             optimizer.step()
 
-            # print statistics
             running_loss += loss.item()
-            if i % 10 == 9:  # print every 10 mini-batches
-                print('[Epoch: %d, Batch: %4d / %4d], loss: %.3f' %
-                      (epoch + 1, i + 1, len(train_loader), running_loss / 10))
+            if i % 10 == 9:
+                avg_loss = running_loss / 10
+                print(f'[Epoch: {epoch + 1}, Batch: {i + 1}/{len(train_loader)}], loss: {avg_loss:.3f}')
+                wandb.log({"train_loss": avg_loss, "epoch": epoch})
                 running_loss = 0.0
 
         pointnet.eval()
         correct = total = 0
+        with torch.no_grad():
+            for data in valid_loader:
+                inputs, labels = data['pointcloud'].to(device).float(), data['category'].to(device)
+                outputs, __, __ = pointnet(inputs.transpose(1, 2))
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-        # validation
-        if valid_loader:
-            with torch.no_grad():
-                for data in valid_loader:
-                    inputs, labels = data['pointcloud'].to(device).float(), data['category'].to(device)
-                    outputs, __, __ = pointnet(inputs.transpose(1, 2))
-                    _, predicted = torch.max(outputs.data, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
-            val_acc = 100. * correct / total
-            print('Valid accuracy: %d %%' % val_acc)
-        # save the model
+        val_acc = 100. * correct / total
+        print(f'Valid accuracy: {val_acc:.2f} %')
+        wandb.log({"val_acc": val_acc, "epoch": epoch})
 
-        checkpoint = Path(args.save_model_path) / 'save_' + str(epoch) + '.pth'
+        checkpoint = Path(cfg.save_model_path) / f'save_{epoch}.pth'
         torch.save(pointnet.state_dict(), checkpoint)
         print('Model saved to ', checkpoint)
 
+    wandb.finish()
 
-if __name__ == '__main__':
-    args = parse_args()
-    train(args)
+
+@hydra.main(config_path="config", config_name="config", version_base="1.3")
+def main(cfg: DictConfig):
+    train(cfg)
+
+if __name__ == "__main__":
+    main()
